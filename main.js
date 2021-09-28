e975390ea57af28ad9ef4d6bd63ad77e021bdee8
priyansh11k <80451561+priyansh11k@users.noreply.github.com>
2021-09-28 11:53:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,5 +18,29 @@ Webcam.set({
     console.log('ml5 version is ', ml5.version);
     classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/IleNyLoVo/model.json');
 
+    function model_loaded(){
+        console.log("model is loaded");
+        }
+        
+        function check(){
+            img = document.getElementById("captured_image");
+            classifier.classify(img,got_Result);
+        }
+        
+        function got_Result(error,results){
+        if (error){
+        console.error(error);
+        }
+        else{
+        console.log(results);
+        document.getElementById("obj_name").innerHTML = results[0].label;
+        document.getElementById("obj_acc").innerHTML = results[0].confidence.toFixed(2);
+        }
+        }
+        
+        
+
+
+
     
     
